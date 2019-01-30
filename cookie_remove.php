da7ed35a8083d6681d6140a064008dbabf999e33
unstartdev <guo.yongan@outlook.com>
2019-01-30 16:32:07 +0800

    垃圾回收

---
+++ cookie_remove.php
@@ -0,0 +1,5 @@
+<?php
+require "./sqlite_dnb.php";
+require "./scripts/flush_cookie.php";
+flush_cookie($xlink);
+$xlink = null;
--- scripts/flush_cookie.php
+++ scripts/flush_cookie.php
@@ -1,2 +1,21 @@
 <?php
-require "../sqlite_dnb.php";
+function flush_cookie($xlink)
+{
+$getsql = 'SELECT TIME FROM SYSTEM_LOGREC ORDER BY TIME;';
+$execsql = $xlink->prepare($getsql);
+$execsql->execute();
+$savetime = $execsql->fetchAll(PDO::FETCH_COLUMN,0);
+$time = time();
+$xlink->beginTransaction();
+foreach ($savetime as $times)
+{
+    if ($times+7200<=$time)
+    {
+        $removesql = 'DELETE FROM SYSTEM_LOGREC WHERE TIME = ' . $times;
+        $row = $xlink->exec($removesql);
+        echo $row;
+    }
+}
+$xlink->commit();
+$xlink=null;
+}
